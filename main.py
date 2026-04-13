80b33f7809f0147c5071a1e25a4988d4ba3c278f
BootcampCoderTF <tmfararira@gmail.com>
2026-04-13 01:42:25 +0100

    Added FPS delta time and comments to explain the code a little

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 def main():
+    # inform the user the game is about to start loading a the size of the window that will open
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    # initialise pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while True:
+    while True: # start an infinite loop
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # creates a empty window
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
